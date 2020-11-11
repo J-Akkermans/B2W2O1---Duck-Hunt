c572d8130ee1cafbc6db69d5e3848009b520170d
J Akkermans <99044668@mydavinci.nl>
2020-11-11 10:33:41 +0100

    aaa

--- script.js
+++ script.js
@@ -1,39 +1,100 @@
-var directions = ["N", "NE","E","SE","S","SW","W","NW"]
+var directions = ["N", "NE", "E", "SE", "S", "SW", "W", "NW"]
 var img = document.getElementById("img");
 
+var step = 75;
 
 
-function fly(direction){
+function fly(direction) {
     direction = direction.toLowerCase();
-    switch(direction) {
+    var top = document.getElementById('img').offsetTop;
+    var left = document.getElementById('img').offsetLeft;
+
+    switch (direction) { //Tussen de haakjes staat de expressie, en case de vergelijking die equal moet zijn.
         case 'n':
-            img.style.top=+ 75 + "px"
-            
-          break;
-        case 'n':
-            alert("north-east")
+            if (top == 0) {
+                alert("test");
+            } else {
+                top = top - step;
+                img.style.top = top + "px";
+            }
             break;
-        case 'n':
-          alert("east")
+        case 'ne':
+            if (top == 0 || left == 1650) {
+                alert('test')
+            } else {
+                left = left + step;
+                img.style.left = left + "px";
+                top = top - step;
+                img.style.top = top + "px";
+            }
+            break;
+        case 'e':
+            if (left == 1650) {
+                alert('test');
+            } else {
+                left = left + step;
+                img.style.left = left + "px";
+            }
             break;
         case 'se':
-            alert("south-east")
+            if (top == 750 || left == 1650) {
+                alert('test')
+            } else {
+                left = left + step;
+                img.style.left = left + "px";
+
+                top = top + step;
+                img.style.top = top + "px";
+            }
             break;
         case 's':
-            alert("south")
+            if (top == 750) {
+                alert('test')
+            } else {
+                top = top + step;
+                img.style.top = top + "px";
+            }
             break;
         case 'sw':
-            alert("south-west")
+            if (top == 750 || left == 0) {
+                alert('test')
+            } else {
+                left = left - step;
+                img.style.left = left + "px";
+
+                top = top + step;
+                img.style.top = top + "px";
+            }
             break;
         case 'w':
-            alert("west")
+            if (left == 0) {
+                alert('test')
+            } else {
+                left = left - step;
+                img.style.left = left + "px";
+            }
             break;
         case 'nw':
-            alert("north-west")
+            if (top == 750 || left == 0) {
+                alert('test')
+            } else {
+                left = left - step;
+                img.style.left = left + "px";
+                top = top - step;
+                img.style.top = top + "px";
+            }
             break;
         default:
-          // code block
-      }
+            break;
+    }
 }
 
-fly('n')
+function moveDuck() {
+    setInterval(function () {
+        //Code dat iedere 0,5 seconden uitgevoerd wordt.
+
+
+    }, 500); //Hoe lang de interval is tussen uitvoering van deze functie.
+}
+
+fly('n');
